b914b4d00d92c37ec8d6ad51f46d2f6ed53ce08f
Demo User <demouser@MacBook-Pro.local>
2015-07-27 14:50:24 -0700

    gmail handler

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ from google.appengine.api import users
 import webapp2
 import jinja2
 from google.appengine.ext import ndb
+from google.appengine.api import urlfetch
+from google.appengine.api import users
 
 env = jinja2.Environment(loader = jinja2.FileSystemLoader('templates'))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = env.get_template('home.html')
-        self.response.write(template.render())
+
+         template = env.get_template('home.html')
+         self.response.write(template.render())
+
 
 class BlurryProfileHandler(webapp2.RequestHandler):
     def get(self):
         template = env.get_template('blurryprofile.html')
         self.response.write(template.render())
+
 
 class MapHandler(webapp2.RequestHandler):
     def get(self):
@@ -22,8 +27,22 @@ class MapHandler(webapp2.RequestHandler):
         self.response.write(template.render())
 
 
+class GmailHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/home')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/home'))
+
+        self.response.out.write("<html><body>%s</body></html>" % greeting)
+
+
 app = webapp2.WSGIApplication([
     ('/home', MainHandler),
     ('/profile', BlurryProfileHandler),
     ('/map', MapHandler)
+    ('/gmail', GmailHandler)
 ], debug=True)
